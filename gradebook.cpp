185850ec6c72748f9a730ef51b47771b1cdb552f
Lexi <lexicara@hotmail.com>
2025-09-23 11:53:33 -0700

    Finished multiplication feature and renamed file to match

---
+++ gradebook.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream> // for files
+#include <string>
+using namespace std; 
+
+int main(){
+   // must read in a file 
+   string filename; 
+   cout << "Enter filename: "; 
+   cin >> filename;
+
+   string line;
+
+    ifstream file(filename); // open the file
+    if(!file.is_open()){
+        cerr << "Error: Could not open file!" << endl;
+        return 1;
+    }
+  
+
+
+
+
+}
