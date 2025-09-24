367cce0b761dee86df8698e99d44a8fbe6ee66af
lexi <lexicara@hotmail.com>
2025-09-23 20:50:00 -0700

    Merge results.txt student.txt gradebook.cpp from multiplication branch

--- gradebook.cpp
+++ gradebook.cpp
@@ -2,23 +2,19 @@
 #include <fstream> // for files
 #include <iomanip> // for setw
 #include <string>
+
 using namespace std; 
 
 int main(){
     // variables
-    int total,testNum;
-    double average;
-    char grade; 
+    int total = 0,testNum = 0;
+    double average = 0.0;
+    char grade, ch; 
 
-   // must read in a file 
-   string filename; 
-   cout << "Enter filename: "; 
-   cin >> filename;
-
+   // must read in/open a file 
+   ifstream file("student.txt"); 
    string line;
 
-   // open the file
-    ifstream file(filename); 
     if(!file.is_open()){
         cerr << "Error: Could not open file!" << endl; // error message
         return 1;
@@ -26,15 +22,20 @@ int main(){
 
 
     // read their fullname from the file
-
-    string line;
-    string student;
+    string student="";
     
-    getline(file,line); // read first line since student name is first
-    student = line; // student is set to this line
+
+    
+    if (file.is_open()) {
+        //Read student name from file into student variable
+        getline(file,student);
+    }
+
+
 
     // now we loop through the text scores until we reach -1
     while(getline(file, line)){ // read each line until end
+        
         if(stoi(line) == -1){ // exit if we reach sentinel value
             break;
         }
@@ -43,38 +44,96 @@ int main(){
     }
 
     // at this point should have all scores loaded up
-    average = total / testNum; // average: total score / # of tests
+        if (testNum > 0) {
+        average = static_cast<double>(total) / testNum;
+        cout << testNum << " tests read." << endl;
 
-    switch(grade){
-        case 1:
-            if(average < 60){
-                grade = 'F';
-            }
-        case 2:
-            if(60 < average < 70){
-                grade = 'D';
-            }
-        case 3:
-            if(70 < average < 80){
-                grade = 'C';
-            } 
-        case 4:
-            if(80 < average < 90){
-                grade = 'B';
-            }  
-        case 5:
-            if(90 < average < 100){
-                grade = 'A';
-            }
-        default:
-            cout << "Something went wrong. No scores read.";     
-    } 
+        } else {
+        cerr << "Error: no tests read (division by zero)" << endl;
+        return 1;
+        }
+
+        
+        if (average >=90){
+        grade = 1;
+        }
+        else if (average >=80){
+            grade = 2;
+        }
+        else if (average >=70){
+            grade = 3;
+        }
+        else if (average >=60){
+            grade = 4;
+        }
+        else if (average <60){
+            grade = 5;
+        }
+
+
+        switch(grade){
+            case 1:{
+                    grade = 'A';
+                    break;
+                }
+            case 2:{
+                    grade = 'B';
+                    break;
+                }
+            case 3:
+                {
+                    grade = 'C';
+                    break;
+                } 
+            case 4:
+                {
+                    grade = 'D';
+                    break;
+                }  
+            case 5:
+                {
+                    grade = 'F';
+                    break;
+                }
+            default:
+                cout << "Something went wrong. No scores read.";     
+        } 
+
+    
+    string status = (average >= 60) ? "PASS" : "FAIL";
     
     
+
     // setup table, use table default box size of 25
-    cout << "__________________________________________________" << endl; // top of box, 50 underscores;
-    cout << setw(50) << "| Student name " << "| Grade |"; // label
-    cout << setw(50) << "| " << student << "| " << grade << " |"; // filled in info
-
+    cout << "----------------------------------------------------" << endl;
+    cout << left << setw(20) << "Student name:" << setw(10) << "Grade" << setw(15) << "Avg Score" << setw(10) << "Status" << endl; // label
+    cout << "----------------------------------------------------" << endl;
+    cout << left << setw(20) << student << setw(10) << grade << setw(15) << average << setw(10) << status << endl; // label    
+    cout << "----------------------------------------------------" << endl;
     
+    cout << "Enter 'Y' to save to a file." << endl;
+    cout << "Otherwise, enter any other key to exit without saving." << endl;
+
+    cin.get(ch);
+
+        if (ch == 'y' || ch == 'Y'){
+            ofstream outFile("results.txt");
+            if (outFile.is_open()){
+                    outFile << "----------------------------------------------------" << endl;
+                    outFile << left << setw(20) << "Student name:" << setw(10) << "Grade" << setw(15) << "Avg Score" << setw(10) << "Status" << endl; // label
+                    outFile << "----------------------------------------------------" << endl;
+                    outFile << left << setw(20) << student << setw(10) << grade << setw(15) << average << setw(10) << status << endl; // label    
+                    outFile << "----------------------------------------------------" << endl;
+                outFile.close();
+                cout << "Reults saved into results.txt" << endl;
+            }
+            else{
+                cerr << "Error: Could not open file for writing!" << endl;
+                return 1;
+            }
+        }
+        else {
+            cout << "Exiting without saving." << endl;
+        }
+        return 0;
 }
