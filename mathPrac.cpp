f2437a060a67f18ff78ab7cfe08fd33e4f0b8dff
PJiggy <pjinocencio@gmail.com>
2025-09-23 21:21:25 -0700

    added all of the math choices and output file. all working to my knowledge after about 5 trial runs.

--- mathPrac.cpp
+++ mathPrac.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <random>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
 int main(){
     // must keep track of right and wrong answers
-    int correct = 0, wrong = 0;
+    double correct = 0, wrong = 0, questions = 0;
     
     // keeps track of the choice for the user
     int choice;
@@ -36,12 +37,28 @@ int main(){
 
         //Addition
         else if (choice == 1){
+            questions++; // add one questions
 
-            
+            int a = distr(gen);
+            int b = distr(gen);
+            int correctAnswer = a + b;
+            int userAnswer;
+
+            cout << "What is " << a << " + " << b << "? ";
+            cin >> userAnswer;
+
+            if (userAnswer == correctAnswer) {
+                cout << "Correct!\n";
+                ++correct;
+            }   else {
+                cout << "Sorry, the right answer is " << correctAnswer << ".\n";
+                ++wrong;
+            }
         }
-
         //Subtraction, Paul
         else if (choice == 2){
+            questions++; // add one question
+
             int minuend = distr(gen); // random number for minuend
             int subtrahend = distr(gen); // random for subtrahend
             int answer;
@@ -61,6 +78,8 @@ int main(){
 
         //Multiplication
         else if (choice == 3){
+            questions++; // add one question
+
             //Generate two numbers
             int a = rand() % 10 + 1;
             int b = rand() % 10 + 1;
@@ -89,43 +108,63 @@ int main(){
 
         //Division
         else if (choice == 4){
-            
-            
+            questions++; // add one question
+
+            int a = rand() % 10 + 1; 
+            int b = rand() % 10 + 1; 
+            int answer = a / b; 
+
+            int userAnswer;
+
+            cout << "What is " << a << " / " << b << "?\n" << endl;
+            cout << "Enter your answer: " << endl;
+            cin >> userAnswer;
+
+            if (userAnswer == answer) {
+            correct++;
+            cout << "Correct!" << endl;
+            } else {
+                wrong++;
+                cout << "Incorrect!" << endl;
+                cout << "Your answer: " << userAnswer << endl;
+                cout << "Correct answer: " << answer << endl;
+                cout << endl << endl;
+            }
+            continue;
         }
         
         //Statistics
         else if (choice == 5){
+            cout << "\n===== Statistics Summary =====\n";
+            cout << setw(20) << left << "Total Questions: " << questions << endl;
+            cout << setw(20) << left << "Correct Answers: " << correct << endl;
+            cout << setw(20) << left << "Wrong Answers: " << wrong << endl;
 
+            double percentage = 0.0;
+    
+            if (questions > 0) {
+                percentage = (correct / questions ) * 100;
+            }
+            cout << setw(20) << left << "Accuracy: " << fixed << setprecision(2) << percentage << "%" << endl;
+
+            ofstream outFile("results.txt", ios::app);
+            if (outFile.is_open()) {
+                outFile << "===== Statistics Summary =====\n";
+                outFile << "Total Questions: " << questions << "\n";
+                outFile << "Correct Answers: " << correct << "\n";
+                outFile << "Wrong Answers: " << wrong << "\n";
+                outFile << "Accuracy: " << fixed << setprecision(2) << percentage << "%\n";
+                outFile << "==============================\n\n";
+                outFile.close();
+                cout << "\n(Statistics saved to results.txt)\n";
+            } else {
+                cout << "\nError: Could not save results to file.\n";
+            }
             
         }
         else {
            cout << "Please enter a valid option. " << endl;
         }
     }
-
-
-
-
-        
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return 0;
 }
